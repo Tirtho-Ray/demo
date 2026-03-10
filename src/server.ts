39d3dd517d55761ebe8c17eed1d769b6f0c246c3
Tirtho ray10 <134957443+Tirtho-Ray@users.noreply.github.com>
2026-03-10 15:11:39 +0600

    Update database connection URL handling

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import app from "./app";
 
 
 const port = process.env.PORT || 8000
-const db = process.env.DB_URL ||"mongodb+srv://lms_project:B6GR1hVvhdIa5uly@cluster0.thodmul.mongodb.net/lms?appName=Cluster0786"
+const db = process.env.DB_URL ||""
 
 async function main() {
   await mongoose.connect(db as string);
